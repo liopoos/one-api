10a2e37dcbee80d7be5b60f1bc89f25e1c78a342
hades <i@mayuko.cn>
2017-04-27 10:32:26 +0800

    Update

--- README.md
+++ README.md
@@ -1,58 +1,173 @@
-#  ONE API #
+#  ONE API（V2） #
 
-**更新：**EveryDay
+ONE·一个API可以获取当天的图片，文字，还可以获取过去7天的文章、Q&A、文字图片等。
 
-**问题反馈：**i@mayuko.cn
+## 实例效果
 
-**数据获取：**数据获取
+[效果页：](http://127.0.0.1/apicenter/d.php?i=1#)[ONE·一个](http://lab.mayuko.cn/one/)
 
-**调用示例：**JavaScript + HTML （同步）
+详细介绍：[ONE·一个API更新计划](https://blog.mayuko.cn/archives/2289)
 
-# 数据获取 #
-
-**请求地址(SSL)：**https://blog.mayuko.cn/api/one-api
-
-**请求方式：GET**
-
-调用举例 – JavaScript + HTML
+API Center定位：[ONE·一个](https://api.mayuko.cn/d.php?i=1)
 
 
-**脚本地址(SSL)：**https://blog.mayuko.cn/api/one-api
+
+## 请求地址
+
+https://api.mayuko.cn/v2/one
 
 
-**图片地址(SSL)：**https://blog.mayuko.cn/api/one-api/img.php
 
-# 实例： #
+## 请求方式
 
-**请求：**https://blog.mayuko.cn/api/one-api
+HTTPS	GET
 
-**返回：**
 
-标题：VOL.1271 何时再见
 
-图片标题：何时再见
+## 请求参数
 
-林友熙 作品
+#### **系统级参数**
 
-内容：即使热恋者的情感是错觉、幻象或自恋行为，那又何妨，所谓人生就是一段不断追求情爱的路程。 by 森山大道
-# 使用方法： #
+所有接入点需要的参数。
 
-**该脚本实质为document.write的脚本。**
+| 参数名称 | 类型     | 示例值                              | 必须   | 说明               |
+| ---- | ------ | -------------------------------- | ---- | ---------------- |
+| SK   | string | c7acff69c5a5acd08fcc4af108b592dd | 必须   | 每一个用户名对应唯一一个SK值。 |
 
-1.将下面这段代码放入HTML页面的head标签内
+#### **应用级参数**
 
-    <script type="text/javascript" src="https://blog.mayuko.cn/api/one-api"></script>
+每个接入点自己的参数。
 
-2.获得图片数据
+| 参数名称  | 类型   | 示例值  | 必须   | 说明                                       |
+| ----- | ---- | ---- | ---- | ---------------------------------------- |
+| date  | int  | 5    | 非必须  | 获取某天的内容，0为当天的内容，1为前一天，2为前两天，最大为6即6天前，默认为0。 |
+| value | int  | 1    | 非必须  | 判定输出内容，0为全部输出，1只输出图文，2只输出文章，3只输出问答，默认为1。 |
 
-    <img src="https://blog.mayuko.cn/api/one-api/img.php" alt="" />
 
-3.获得标题、内容：
 
-标题：`<div id="onetitle"><script>onetitle()</script></div>`
+## 返回参数
 
-图片标题：`<div id="oneimga"><script>oneimga()</script></div>`
+以JSON格式返回结果。
 
-内容：`<div id="oneword"><script>oneword()</script></div>`
+#### **系统级参数**
 
-尽情享受吧~ 
+所有接入点需要的参数。
+
+| 参数名称 | 类型     | 说明                 |
+| ---- | ------ | ------------------ |
+| code | string | 1：正常-1：SK错误-2：参数错误 |
+
+#### **应用级参数**
+
+每个接入点自己的参数。
+
+| 参数名称             | 类型     | 说明     |
+| ---------------- | ------ | ------ |
+| code             | int    | 返回码    |
+| titulo_title     | string | 图文标题   |
+| titulo_img_url   | string | 图文图片地址 |
+| titulo_img_title | string | 图文图片标题 |
+| titulo_content   | string | 图文正文   |
+| article_title    | string | 文章标题   |
+| article_autor    | string | 文章作者   |
+| article_content  | string | 文章正文   |
+| question_title   | string | 问答标题   |
+| question_q       | string | 问答问题   |
+| question_a       | string | 问答答案   |
+
+
+
+## 请求实例
+
+以 PHP 为例的请求实例。
+
+```php
+$sk = '';
+$url = "https://api.mayuko.cn/v2/one?sk=sk&date=2";
+echo get_file($url);
+function get_file($url)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
+                    
+                
+```
+
+以 JAVA 为例的请求实例。
+
+```java
+public static void main(String path[]) throws Exception {
+    URL u=new URL("https://api.mayuko.cn/v2/one?sk=sk&date=2");
+    InputStream in=u.openStream();
+    ByteArrayOutputStream out=new ByteArrayOutputStream();
+    try {
+        byte buf[]=new byte[1024];
+        int read = 0;
+        while ((read = in.read(buf)) > 0) {
+            out.write(buf, 0, read);
+        }
+    } finally {
+        if (in != null) {
+            in.close();
+        }
+    }
+    byte b[]=out.toByteArray( );
+    System.out.println(new String(b,"utf-8"));
+}
+                    
+                
+```
+
+以 Python 为例的请求实例。
+
+```python
+import urllib.parse
+import urllib.request
+
+data={}
+data['sk']=''
+data['参数']=''
+url_values=urllib.parse.urlencode(data)
+url = 'https://api.mayuko.cn/v2/one?sk=sk&date=2?'
+full_url=url+url_values
+data=urllib.request.urlopen(full_url).read()
+z_data=data.decode('UTF-8')
+print(z_data)
+                    
+                
+```
+
+
+
+## 返回实例
+
+以JSON格式返回结果。
+
+```json
+{
+“code”:1,
+“titulo_title”:“”,
+“titulo_img_url”:“”,
+“titulo_img_title”:“”,
+“article_title”:“”,
+“article_autor”:“”,
+“article_content”:“”,
+“question_title”:“”,
+“question_q”:“”,
+“question_a”:“”
+}
+```
+
+
+
+## LICENSE
+
+MIT © [Hades](http://github.com/mayuko2012)
--- index.php
+++ index.php
@@ -1,29 +1,86 @@
 <?php
+$type = $_GET['type'];
+$date = $_GET['date'];
+$value = $_GET['value'];
+if (!isset($type)) {
+    $type = 'json';
+}
+if (!isset($date)) {
+    $date = 0;
+}
+if (!isset($value)) {
+    $value = 1;
+}
+get_result($date, $type, $value);
+function get_result($date, $type, $value)
+{
 //准备对接
-header('Content-type: application/x-javascript');
-header("Content-type: text/html; charset=utf-8");
+    header('Content-type: application/x-javascript');
+    header("Content-type: text/html; charset=utf-8");
 //对接完成
 //准备接收数据
-$url='http://wufazhuce.com';//one一个url
-$data=get_file($url);
+    $url = 'http://wufazhuce.com';//one一个url
+    $data = get_file($url);
 //数据接收完成
 //准备分析数据
-$title='/(?<=(<p class="titulo">))[^<]*(?=(<\/p>))/';//匹配标题
-$num_1=preg_match_all($title,$data,$match_title);
-$imgurl='/(?<=(<img class="fp-one-imagen" src="))[^<]*(?=(" alt="" \/><\/a> ))/';//匹配图片url
-$num_2=preg_match_all($imgurl,$data,$match_imgurl);
-$img_title='/(?<=( ))[^>]*(?=(&))/';//匹配图片标题
-$num_3=preg_match_all($img_title,$data,$match_imgtitle);
-$img_author='/(?<=(&amp;))[^<]*(?=(<\/div>))/';//匹配图片作者
-$num_4=preg_match_all($img_author,$data,$match_imgauthor);
-$word='/(?<=(<a href="http:\/\/wufazhuce.com\/one\/)\d{4}">)[^>]*(?=(<))/';//匹配文字
-$num_5=preg_match_all($word,$data,$match_word);
-//数据分析完成
-//准备处理数据
-echo "function onetitle(){document.write(\"" . $match_title[0][0] . "\");}";
-echo "function oneimga(){document.write(\"" . $match_imgtitle[0][0].'<br>'.$match_imgauthor[0][0] . "\");}";
-echo "function oneword(){document.write(\"" . $match_word[0][1] . "\");}";
+    $article_url = $article_url[0][$date];
+    $article_data = get_file($article_url);
+    preg_match('/(?<=<h2 class="articulo-titulo">).*?(?=<\/h2>)/si', $article_data, $article_title);//匹配文章标题
+    preg_match('/(?<=<p class="articulo-autor">).*?(?=<\/p>)/si', $article_data, $article_autor);//匹配文章作者
+    preg_match('/(?<=<div class="articulo-contenido">).*?(?=<\/div>)/si', $article_data, $article_content);//匹配文章内容
 
+    $question_url_num = preg_match_all('/(http:\/\/wufazhuce.com\/question\/\d{4})/si', $data, $question_url);//匹配文章url
+    $question_url = $question_url[0][$date];
+    $question_data = get_file($question_url);
+    preg_match('/(?<=<h4>).*?(?=<\/h4>)/si', $question_data, $question_title);//匹配问答标题
+    preg_match_all('/(?<=<div class="cuestion-contenido">).*?(?=<\/div>)/si', $question_data, $question_qa);//匹配文章作者
+
+
+//数据分析完成
+//准备连接数据库
+//准备处理数据
+    if ($type == 'json') {
+        $result = array();
+        $result['code'] = 1;
+        if ($value == 0) {
+            $result['titulo_title'] = $title[0][$date];
+            $result['titulo_img_url'] = $img_url[0][$date];
+            $result['titulo_img_title'] = $img_title[0][$date];
+            $content = preg_replace('/<br \/>/', '', $content[0][($date * 2) + 1]);
+            $result['titulo_content'] = $content;
+            $result['article_title'] = $article_title[0];
+            $result['article_autor'] = $article_autor[0];
+            $result['article_content'] = $article_content[0];
+            $result['question_title'] = $question_title[0];
+            $result['question_q'] = $question_qa[0][0];
+            $result['question_a'] = $question_qa[0][1];
+        } elseif ($value == 1) {
+            $result['titulo_title'] = $title[0][$date];
+            $result['titulo_img_url'] = $img_url[0][$date];
+            $result['titulo_img_title'] = $img_title[0][$date];
+            $content = preg_replace('/<br \/>/', '', $content[0][($date * 2) + 1]);
+            $result['titulo_content'] = $content;
+        } elseif ($value == 2) {
+            $result['article_title'] = $article_title[0];
+            $result['article_autor'] = $article_autor[0];
+            $result['article_content'] = $article_content[0];
+        } elseif ($value == 3) {
+            $result['question_title'] = $question_title[0];
+            $result['question_q'] = $question_qa[0][0];
+            $result['question_a'] = $question_qa[0][1];
+        } else {
+            $code['code'] = -2;
+            $code['status'] = 'parameter[value] error';
+            echo json_encode($code);
+        }
+
+        echo json_encode($result);
+    } else {
+        $code['code'] = -2;
+        $code['status'] = 'parameter[type] error';
+        echo json_encode($code);
+    }
+}
 
 
 
@@ -31,12 +88,13 @@ echo "function oneword(){document.write(\"" . $match_word[0][1] . "\");}";
 function get_file($url)
 {
     $curl = curl_init();
-    curl_setopt($curl,CURLOPT_URL,$url);  
+    curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HEADER, 0);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($curl);  
+    $data = curl_exec($curl);
     curl_close($curl);
     return $data;
 }
+
 //对接成功
 ?>
